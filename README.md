acb01880c41b225f5b920554722cf523332fd68d
lucio-crDEV <lucio.m.cardenas@gmail.com>
2023-05-27 05:03:01 -0400

    version actualizada que devuelve tabla por terminal

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
-# Script para obtener todas las comunas por región
+# Comunas por Región en Chile
 
-clona el repo y ejecuta el main.js con
-```
-node main.js
-```
+Este script de Node.js muestra una tabla con las regiones de Chile y la lista de comunas para cada región, obteniendo los datos de la API de las regiones y comunas proporcionada por el Gobierno de Chile.
+
+## Requisitos
+
+- Node.js (v14 o superior)
+
+## Instalación
+
+1. Clona este repositorio o descarga los archivos del script.
+
+2. Abre una terminal y navega hasta el directorio del proyecto.
+
+3. Ejecuta el siguiente comando para instalar las dependencias:
+
+   ```
+   bash
+   npm install
+   ```
+
+## Uso
+
+1. Abre una terminal y navega hasta el directorio del proyecto.
+
+2. Ejecuta el siguiente comando para ejecutar el script:
+
+    ```
+    node mains.js
+    ```
+
+3. Verás la tabla con las regiones y comunas de Chile.
--- mains.js
+++ mains.js
@@ -1,36 +1,53 @@
+const Table = require('cli-table3');
+const clc = require('cli-color');
+
 // Función para obtener los códigos de las regiones
 const urlRegiones = "https://apis.digital.gob.cl/dpa/regiones";
-function obtenerCodigosRegiones(regiones) {
-    var codigos = [];
-    for (var i = 0; i < regiones.length; i++) {
-      var region = regiones[i];
-      codigos.push(region.codigo);
-    }
-    return codigos;
-  }
+
+function obtenerComunasPorRegion(region) {
+  const urlComunas = `${urlRegiones}/${region.codigo}/comunas`;
+
+  return fetch(urlComunas)
+    .then(response => response.json())
+    .then(comunas => {
+      const nombresComunas = comunas.map(comuna => comuna.nombre);
+      const comunasSeparadas = nombresComunas.join("\n"); // Añadir salto de línea entre las comunas
+      return {
+        region: region.nombre,
+        comunas: comunasSeparadas,
+        cantidad: nombresComunas.length
+      };
+    });
+}
+
 fetch(urlRegiones)
   .then(response => response.json())
   .then(regiones => {
-    // Obtener los códigos de las regiones
-    var codigosRegiones = obtenerCodigosRegiones(regiones);
-    // Imprimir una respuesta por cada código de región
-    console.log("Comunas por región:");
-    codigosRegiones.forEach(codigo => {
-      const urlComunas = urlRegiones + "/" + codigo + "/comunas";
-      let acum = [];
+    const promesasComunas = regiones.map(region => obtenerComunasPorRegion(region));
 
-      fetch(urlComunas)
-        .then(response => response.json())
-        .then(comunas => {
-          console.log('\n')
-          console.log(`Región: ${comunas[0].nombre}`);
-          for (let i = 0; i < comunas.length; i++) {
-              acum.push(`${comunas[i].nombre}`);
+    Promise.all(promesasComunas)
+      .then(resultados => {
+        const table = new Table({
+          head: [clc.red('Región'), clc.red('Comunas'), clc.red('Cantidad \nde comunas')],
+          colWidths: [40, 25, 13],
+          chars: {
+            'top': '-', 'top-mid': '-', 'top-left': '-', 'top-right': '-',
+            'bottom': '_', 'bottom-mid': '_', 'bottom-left': '_', 'bottom-right': '_',
+            'left': '|', 'left-mid': '|', 'mid': '_', 'mid-mid': '|',
+            'right': '|', 'right-mid': '|', 'middle': '|' // Espacio en lugar de línea vertical
           }
-          console.log(`Comunas: " ${acum} "`);
-          console.log(`Cantidad de comunas: ${acum.length}`);
-        })
-        .catch(e => console.log(e));
-    });
+        });
+
+        resultados.forEach(resultado => {
+          table.push([
+            clc.cyan(resultado.region),
+            clc.yellow(resultado.comunas),
+            clc.green(resultado.cantidad)
+          ]);
+        });
+
+        console.log(table.toString());
+      })
+      .catch(e => console.log(e));
   })
   .catch(e => console.log(e));
