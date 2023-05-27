353b469f0f2029f46ead1133ef1946fddce6d5f4
Lucio <78000598+lucio-crDEV@users.noreply.github.com>
2023-05-26 21:02:22 -0400

    Update and rename consulta.js to mains.js
    actualización que responde todas las comunas por región en una sola ejecucion

---
+++ mains.js
@@ -0,0 +1,45 @@
+// Función para obtener los códigos de las regiones
+function obtenerCodigosRegiones(regiones) {
+    var codigos = [];
+  
+    for (var i = 0; i < regiones.length; i++) {
+      var region = regiones[i];
+      codigos.push(region.codigo);
+    }
+  
+    return codigos;
+  }
+  
+  const urlRegiones = "https://apis.digital.gob.cl/dpa/regiones";
+  
+  fetch(urlRegiones)
+    .then(response => response.json())
+    .then(regiones => {
+      // Obtener los códigos de las regiones
+      var codigosRegiones = obtenerCodigosRegiones(regiones);
+  
+      // Imprimir una respuesta por cada código de región
+      console.log("Respuestas por cada código de región:");
+  
+      codigosRegiones.forEach(codigo => {
+        const urlComunas = urlRegiones + "/" + codigo + "/comunas";
+        let acum = [];
+  
+        fetch(urlComunas)
+          .then(response => response.json())
+          .then(comunas => {
+            console.log('\n')
+            console.log(`Región: ${comunas[0].nombre}`);
+            for (let i = 0; i < comunas.length; i++) {
+                acum.push(`${comunas[i].nombre}`);
+            }
+            console.log(`Comunas: " ${acum} "`);
+            console.log(`Cantidad de comunas: ${acum.length}`);
+  
+            // Agrega aquí la lógica adicional que desees ejecutar para cada código de región
+          })
+          .catch(e => console.log(e));
+      });
+    })
+    .catch(e => console.log(e));
+  
